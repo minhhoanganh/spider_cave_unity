d74675522736ad44e66b17d3bf42286589d12613
minhhoanganh <mrminhhoanganh@gmail.com>
2019-07-10 22:50:45 +0700

    add Sence MainMenu + LevelMenu + Script Controller for 2 Sence

---
+++ Assets/Scripts/LevelMenuCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenuCtrl : MonoBehaviour
+{
+    public void LevelPlayButton()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void BackToMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
